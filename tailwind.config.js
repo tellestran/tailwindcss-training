2dfa9fc6846201d07bc34427c8db60be79f69ac9
tellestran <tranngoctien084@gmail.com>
2023-09-23 08:51:50 +0700

    first add

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "jit",
+  purse:["./*.html"],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
